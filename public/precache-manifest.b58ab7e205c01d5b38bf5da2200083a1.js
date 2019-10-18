c26e60d1ddf2058967a68be1c1d94b03acb6af24
Daniel Garcia <jdanielink@gmail.com>
2019-10-18 02:02:28 -0500

    Updated react build

---
+++ public/precache-manifest.b58ab7e205c01d5b38bf5da2200083a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1812715d309d2a913e672be610a685cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d50e451002ce26d407c",
+    "url": "/static/css/main.d21291e1.chunk.css"
+  },
+  {
+    "revision": "3559f0ec25bad786f7ac",
+    "url": "/static/js/2.ae2ca9a7.chunk.js"
+  },
+  {
+    "revision": "9d50e451002ce26d407c",
+    "url": "/static/js/main.d8cf46e3.chunk.js"
+  },
+  {
+    "revision": "d1dd6c53aaf72e0715cc",
+    "url": "/static/js/runtime-main.6dd21995.js"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.99031dfbb5904f9812a97e12156267f5.js"
+  "/precache-manifest.b58ab7e205c01d5b38bf5da2200083a1.js"
 );
 
 self.addEventListener('message', (event) => {
